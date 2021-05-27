394cd8861407aa94153196367720ba307ed9d437
SmithRBG <smit36000@gmail.com>
2021-05-27 15:41:27 -0400

    updating files

--- javascript.js
+++ javascript.js
@@ -18,10 +18,12 @@ start.onclick = () => {
 //put div around landing, and then onclick hide landing and start quiz
 
 
-const questions = document.getElementById("question-title"); //style
-const choices = document.getElementById("questions");//style
-const index = 0
+//Cycling through the questions
 
+var questions = document.getElementById("question-title"); //style
+var choices = document.getElementById("questions");//style
+var index = 0 //this will start everything at 0, and you can add to it depending on how you need it in the function
+var countdown = document.querySelector(".timer")
 
 function getQuestion() {
    const currentQ = question [index]
@@ -31,25 +33,28 @@ function getQuestion() {
       const choiceBtn = document.createElement("button")
       choiceBtn.setAttribute("class", "choice") //assigning class of choice to the button
       choiceBtn.setAttribute("value", choice) //value coming from below (choices section in each question)
-      choiceBtn.textContent = i + 1 + "" + choice
+      choiceBtn.textContent = i + 1 + ". " + choice
+      choices.append(choiceBtn)//putting text on the buttons
       choiceBtn.onclick = answerSelect
    })
 }
 
-/* function getChoices() {
-   const currentChoice = question [index]
-   choices.textContent = currentChoice.choice
-   choices.innerHTML
-   
-
-}
- */
-
-function answerSelect() { //this is talking about question var
-   if(this.value !== question[index].answer) {
+function answerSelect() { 
+   if(this.value !== question[index].answer) { //this is talking about question var
       countdown -= 10 //this is taking 10 seconds away, when the wrong answer is pressed
+      alert ("Oops, looks like that was the wrong answer partner.")
    }
+   else if (this.value === question[index].answer) { //this is grabbing the below
+      alert ("It's like you're some kind of post-punk magician!")
+   }
+
    index++ //This will cycle to the next question
+   if(index === question.length) { //once the length of all the questions have been cycled through it will end.
+      quizOver()
+   }
+   else { //if all of the questions have not been cycled through, then the get the next object in te array of questions (above)
+      getQuestion()
+   }
 }
 
 
@@ -58,39 +63,42 @@ var question = [
    {
       title:"What is Bauhaus' best known song?", 
       choices: ["Bela Lugoshi's Dead", "In The Flat Field", "All We Ever Wanted Was Everything"],
-      answer: "answer 1"
+      answer: "Bela Lugoshi's Dead"
    },
 
    {
       title:"Which below is a song by The Cure?", 
       choices: ["I'm Shy", "Just Like Heaven", "Friday's"],
-      answer: "answer 2"
+      answer: "Just Like Heaven"
    },
 
    {
       title:"HIM is from which country?", 
       choices: ["England", "Finland", "Norway"],
-      answer: "answer 2"
+      answer: "Finland"
    },
 
    {
       title:"Who is considered a pioneer in the post punk/goth genre?", 
       choices: ["Siouxsie Sioux and the Banshees", "Type O Negative", "ACDC"],
-      answer: "answer 1"
+      answer: "Siouxsie Sioux and the Banshees"
    },
 
    {
       title:"Joy Divison is known for which song?", 
       choices: ["Isolation", "Decades", "Love will Tear Us Apart "],
-      answer: "answer 3"
+      answer: "Love will Tear Us Apart "
    },
 ]
+
+
+//Start Button to get questions
 
 var startBtn = document.querySelector("#startBtn");
 var startContainer = document.querySelector(".start_container");
 var questionBox = document.querySelector(".questionBox");
 
-startBtn.addEventListener('click', start);
+startBtn.addEventListener('click', start); //start button to work
 
 function start() {
    startContainer.style.display = "none";
@@ -98,7 +106,22 @@ function start() {
    getQuestion();
 };
 
+//End Screen
 
+function quizOver() {
+   console.log("test")
+   var endScreen = document.querySelector(".endBox")
+   endScreen.removeAttribute("class")
+   //endScreen.style.display = endScreen.style.display === "none"?"":"block"
+   var qSection = document.querySelector(".qSection")
+   qSection.setAttribute("class", "hide")
+   //qSection.addClass("display", "none")
+}
+
+
+
+
+//high score area WIP
 var endBox = document.getElementsByClassName("endBox");
 var nextBtn = document.getElementsByClassName("next")
 
